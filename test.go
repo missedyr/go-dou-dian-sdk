b00c27a4664e6ea4f5371c628ac38f3a9c085ccd
xuexin <xuexin@guanghe.tv>
2021-10-26 16:49:14 +0800

    update-readme

--- test.go
+++ test.go
@@ -3,9 +3,9 @@ package test
 //func main()  {
 //	appKey := "7022895037826467361"
 //	appSecret := "21a8a717-fc87-4e21-8836-7e052b4f4f8a"
-//	shopId := "11396413"
-//	orderIdParent := "4849294789047992279"
+//	//shopId := "11396413"
+//	//orderIdParent := "4849294789047992279"
 //	log.Infof("1111111")
-//	accDa, err:= douDianSdk.OrderDetail(appKey, appSecret, shopId, orderIdParent)
+//	accDa, err:= douDianSdk2.GetAccessToken(appKey, appSecret, "")
 //	log.Infof("2222222", accDa, err)
 //}
